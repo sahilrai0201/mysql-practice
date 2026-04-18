2462914a19a639878ca41c54be6e1b327f7eb104
sahilrai0201 <sahilatwork21@gmail.com>
2026-04-18 20:09:50 +0530

    Initial commit: Add SQL folder

---
+++ queries.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS college;
+USE college;
+
+CREATE TABLE student(
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+SELECT * FROM student;
+
+INSERT INTO student 
+(rollno, name)
+VALUES
+(101, "Karan"),
+(102, "Arjun"),
+(103, "Ram");
+
+INSERT INTO student VALUES (104, "Shyam");
