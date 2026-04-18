415ae0fc85074d01472da63a432430b03dbdf010
sahilrai0201 <sahilatwork21@gmail.com>
2026-04-18 22:39:04 +0530

    Added one more practice file

---
+++ practice2.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE college;
+USE college;
+
+CREATE TABLE student(
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+INSERT INTO student 
+(rollno, name, marks, grade, city)
+VALUES
+(101, "Anil", 78, "C", "Pune"),
+(102, "Bhumika", 93, "A", "Mumbai"),
+(103, "Chetan", 85, "B", "Mumbai"),
+(104, "Dhruv", 96, "A", "Delhi"),
+(105, "Emanuel", 12, "F", "Delhi"),
+(106, "Farah", 82, "B", "Delhi");
+
+
