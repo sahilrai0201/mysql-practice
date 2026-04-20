191714302f4dd0b18460b6fa142230f0dec3487b
sahilrai0201 <sahilatwork21@gmail.com>
2026-04-20 12:30:10 +0530

    Updated SQL queries and added filtering/order by examples

--- practice2.sql
+++ practice2.sql
@@ -19,4 +19,31 @@ VALUES
 (105, "Emanuel", 12, "F", "Delhi"),
 (106, "Farah", 82, "B", "Delhi");
 
+-- SELECT name, marks FROM student;
+SELECT * FROM student;
+
+-- SELECT city FROM student;
+SELECT DISTINCT city FROM student;
+
+SELECT * 
+FROM student 
+WHERE marks > 80;
+
+SELECT * FROM student WHERE city = "Mumbai";
+
+SELECT * FROM student WHERE marks > 80 AND city = "Mumbai";
+SELECT * FROM student WHERE marks > 80 OR city = "Mumbai";
+
+SELECT * FROM student WHERE marks + 10 > 100;
+
+SELECT * FROM student WHERE marks BETWEEN 80 AND 90;
+
+SELECT * FROM student WHERE city IN ("Pune", "Mumbai", "Gurgaon");
+SELECT * FROM student WHERE city NOT IN ("Pune", "Mumbai", "Gurgaon");
+
+SELECT * FROM student LIMIT 3;
+SELECT * FROM student WHERE  marks > 75 LIMIT 3;
+
+SELECT * FROM student ORDER BY city ASC;
+SELECT * FROM student ORDER BY marks DESC LIMIT 3;
 
